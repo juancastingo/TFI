15fef759112da80f1ba166cb87f37ba6bc0375ac
Juan Cruz Windows 7 virtual <juanxstingo@gmail.com>
2015-11-16 23:30:45 -0300

    commit

---
+++ TFITest4/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TFITest4.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /Abot/
+
+        public ActionResult siteMap()
+        {
+            return View();
+        }
+
+    }
+}
